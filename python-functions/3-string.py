18ea8ff96100e0d3daaac87e39a288f6af153627
mide david <midebabalola7@gmail.com>
2023-07-20 18:20:38 +0000

    String reversion Function

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 def reverse_string(string):
     strings = list(string)
     count = len(strings)
     toreturn = []
     for i in range(count - 1, -1, -1):
-        toreturn.append(strings[add])
-    final = .join(toreturn)
-return final
+        toreturn.append(strings[i])
+    final = "".join(toreturn)
+    return final
