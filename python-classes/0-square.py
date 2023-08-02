3fccdb2136102d34117609b60f13b9aa4960b23c
mide david <midebabalola7@gmail.com>
2023-08-02 12:28:03 +0000

    add proper doc-string

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,12 @@
+"""
+This module defines a square class for creating and printing squares 
+"""
 class Square:
-    """This creates a square of any size"""
+    """This creates a square of any size
+
+    Attributes:
+        size(int): The size of the square's side
+
+    """
     def __init__(self, size=None):
         self.__size = size
