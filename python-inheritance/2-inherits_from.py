079d9b7f90d85580832f578e3784b70128da2fd8
mide david <midebabalola7@gmail.com>
2023-08-03 09:32:46 +0000

    check if obj is a subclass

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,18 @@
+"""
+This module checks if an object is an instance of a specified class
+of its subclass
+
+"""
+def inherits_from(obj, a_class):
+    """
+    Check if the given object is an instance of the specified class or its subclass.
+
+    Parameters:
+        obj: Any object - The object to be checked.
+        a_class: type - The class to compare the object's type with.
+
+    Returns:
+        bool: True if the object is an instance of the specified class or its subclass;
+              False otherwise.
+    """
+    return isinstance(obj, a_class) and issubclass(type(obj), a_class)
