244fe3d0afcbb7c6e4b5826b0bf203e80e293277
mide david <midebabalola7@gmail.com>
2023-08-10 10:47:17 +0000

    add getter and setter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -36,3 +36,112 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+    @property
+    def width(self):
+        """int: The width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set the width of the rectangle.
+
+        Args:
+            value (int): The width value to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is not valid (e.g., <= 0).
+
+        Returns:
+            None
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """int: The height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set the height of the rectangle.
+
+        Args:
+            value (int): The height value to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is not valid (e.g., <= 0).
+
+        Returns:
+            None
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """int: The x offset of the rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Set the x offset of the rectangle.
+
+        Args:
+            value (int): The x value to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is not valid (e.g., < 0).
+
+        Returns:
+            None
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
+    @property
+    def y(self):
+        """int: The y offset of the rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Set the y offset of the rectangle.
+
+        Args:
+            value (int): The y value to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is not valid (e.g., < 0).
+
+        Returns:
+            None
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
