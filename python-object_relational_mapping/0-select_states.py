da1165d79ae4a4414cba3cd0bbdde99cc98aa60b
mide david <midebabalola7@gmail.com>
2023-08-24 19:03:55 +0000

    fix error

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,16 +8,21 @@ import sys
 if __name__ == "__main__":
 
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database_name>".format(sys.argv[0]))
+        print(f"Usage: {sys.argv[0]} <username> <password> <database_name>")
         sys.exit(1)
-
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     try:
-        db = MySQLdb.connect(user=username, host="localhost", port=3306, passwd=password, db=database, )
+        db = MySQLdb.connect(
+                user=username,
+                host="localhost",
+                port=3306,
+                passwd=password,
+                db=database
+                )
         cursor = db.cursor()
     except MySQLdb.Error as e:
         print("Error connecting to the database:", e)
