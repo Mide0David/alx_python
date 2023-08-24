b14ba62fd7e0e3aaa49d20ef4f6fa76ba785ba50
mide david <midebabalola7@gmail.com>
2023-08-24 18:58:23 +0000

    get names in database

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module list all the states in a database
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(user=username, host="localhost", port=3306, passwd=password, db=database, )
+        cursor = db.cursor()
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+        sys.exit(1)
+
+    query = "SELECT * FROM states ORDER by id ASC"
+    try:
+        cursor.execute(query)
+        states = cursor.fetchall()
+    except MySQLdb.Error as e:
+        print("Error executing query:", e)
+        db.close()
+        sys.exit(1)
+
+    for state in states:
+        print(state)
+
+    db.close()
