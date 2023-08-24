6d9749172db3ccf6bde330f714cf183ebfaa91b8
mide david <midebabalola7@gmail.com>
2023-08-24 19:06:09 +0000

    filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} <username> <password> <database_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(
+                user=username,
+                port=3306,
+                host="localhost",
+                passwd = password,
+                db=database
+                )
+        cursor = db.cursor()
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+        sys.exit(1)
+
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+
+    try:
+        cursor.execute(query)
+        states = cursor.fetchall()
+    except MySQLdb.Error as e:
+        print("Error executing query", e)
+        db.close()
+        sys.exit(1)
+    
+    for state in states:
+        print(state);
+
+    db.close()
