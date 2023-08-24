a3653c13da97abbc7f37909811643bfd2d89b7f9
mide david <midebabalola7@gmail.com>
2023-08-24 19:28:33 +0000

    fix case insensitive

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
         print("Error connecting to the database:", e)
         sys.exit(1)
 
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
 
     try:
         cursor.execute(query)
