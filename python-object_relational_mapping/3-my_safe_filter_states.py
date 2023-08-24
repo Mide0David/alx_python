2beff4419970ee4107824649e6881f4069be1072
mide david <midebabalola7@gmail.com>
2023-08-24 20:44:41 +0000

    fix sql injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print(f"Usage: {sys.argv[0]} <username> <password> <database_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    ns = sys.argv[4]
+
+    try:
+        db = MySQLdb.connect(
+                user=username,
+                port=3306,
+                host="localhost",
+                passwd=password,
+                db=database
+                )
+        cursor = db.cursor()
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+        sys.exit(1)
+
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+
+    try:
+        cursor.execute(query, (state_name,))
+        states = cursor.fetchall()
+    except MySQLdb.Error as e:
+        print("Error executing query", e)
+        db.close()
+        sys.exit(1)
+
+    for state in states:
+        print(state)
+
+    db.close()
