ce1634bc4b4d4a693238437ab4e6cfdd7e0efca3
david Babalola <midebabalola7@gmail.com>
2023-10-19 09:49:29 +0000

    fixed issue

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,20 +1,14 @@
 """
-    doc:
-    This module contains an empty class
+This module checks if an object is an instance of a specified class
+of its subclass
+
 """
 class BaseGeometry:
     """
-    Base class for geometry-related objects.
+        A simple base class for geometric objects.
 
-    This class serves as the foundation for defining various geometric shapes
-    and their corresponding properties and methods.
-
-    It currently doesn't have any attributes or methods defined.
-
-    Attributes:
-        None
-
-    Methods:
-        None
+        This class serves as a base for other geometric classes and does not
+        contain any specific attributes or methods.
     """
-        pass
+    pass
+    
