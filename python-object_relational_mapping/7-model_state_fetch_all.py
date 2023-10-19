842b7b10ba941c9bd9407b10c5d6e7ed5d81af48
david Babalola <midebabalola7@gmail.com>
2023-10-19 11:50:15 +0000

    fixed issue

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-"""Define a State class and create a connection to a MySQL database"""
+"""List all State objects from the database"""
 
 import sys
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-Base = declarative_base()
+if __name__ == "__main":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
 
-class State(Base):
-    """State class"""
-    __tablename__ = 'states'
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
-
-if __name__ == "__main__":
     # Create the engine and bind it to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}', pool_pre_ping=True)
 
-    # Create all tables in the database
-    Base.metadata.create_all(engine)
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query and print all State objects
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    # Close the session
+    session.close()
