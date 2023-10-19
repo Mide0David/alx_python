a841d53236349d08d2cecadfccbb144c67d0fe3c
david Babalola <midebabalola7@gmail.com>
2023-10-19 11:42:32 +0000

    fixed issue

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""List all State objects from the database"""
+"""Define the State class and create the corresponding table in the database."""
 
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
-if __name__ == "__main":
+Base = declarative_base()
+
+class State(Base):
+    """State class representing the 'states' table."""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
     # Create the engine and bind it to the database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
-    # Create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    # Create all tables in the database
+    Base.metadata.create_all(engine)
 
-    # Query and print all State objects
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-
-    # Close the session
-    session.close()
