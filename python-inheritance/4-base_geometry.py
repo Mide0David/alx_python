97d339cf6d904887a4991881084efd86b7745bd8
david Babalola <midebabalola7@gmail.com>
2023-10-19 10:54:02 +0000

    fixed improve geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -4,7 +4,25 @@
 
 
 """
-class BaseGeometry:
+
+class BaseGeometryMeta(type):
+    """
+    This is a metaclass used to represent the class type in order to eliminate
+    the inherited method init subclass
+
+        Exclude attribute init subclass in dir()
+
+    """
+
+    def __dir__(cls):
+        attributes = super().__dir__()
+
+        return [
+            attribute for attribute in attributes if attribute != "__init_subclass__"
+        ]
+
+class BaseGeometry(metaclass=BaseGeometryMeta):
+
     """
     Base class for geometry-related objects.
 
@@ -12,6 +30,8 @@ class BaseGeometry:
     and their corresponding properties and methods.
 
     Methods:
+        __dir__(self) -> None
+            Exclude attribute init subclass in dir()
         area(self) -> None
             Placeholder method to calculate the area of the geometric shape.
 
@@ -20,5 +40,14 @@ class BaseGeometry:
                            should be overridden in subclasses to provide specific
                            implementations for calculating the area of a particular shape.
     """
+
+     def __dir__(self):
+
+        attributes = super().__dir__()
+
+        return [
+            attribute for attribute in attributes if attribute != "__init_subclass__"
+        ]
+
     def area(self):
         raise Exception("area() is not implemented")
