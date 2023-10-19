9f027da0f51d07c2bc0500f7fd4765bfa25e525e
david Babalola <midebabalola7@gmail.com>
2023-10-19 11:52:08 +0000

    fixed issue

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""List all State objects from the database"""
+"""Print the first State object from the database"""
 
 import sys
 from sqlalchemy import create_engine
@@ -7,20 +7,27 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
     # Create the engine and bind it to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}', pool_pre_ping=True)
 
     # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query and print all State objects
-    states = session.query(State).order_by(State.id).all()
+    # Query and print the first State object
+    first_state = session.query(State).order_by(State.id).first()
 
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
 
     # Close the session
     session.close()
+
