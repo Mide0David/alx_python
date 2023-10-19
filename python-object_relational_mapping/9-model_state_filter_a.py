9628eed025ed33d0eb5f2bdd75edf8eee62a8157
david Babalola <midebabalola7@gmail.com>
2023-10-19 11:55:32 +0000

    fixed issue

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Print the first State object from the database"""
+"""List all State objects containing the letter 'a' from the database"""
 
 import sys
 from sqlalchemy import create_engine
@@ -7,22 +7,25 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
     # Create the engine and bind it to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}', pool_pre_ping=True)
 
     # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query and print the first State object
-    first_state = session.query(State).order_by(State.id).first()
+    # Query and print State objects containing the letter 'a'
+    states_with_a = session.query(State).filter(State.name.like("%a%")).order_by(State.id).all()
 
-    if first_state:
-        print("{}: {}".format(first_state.id, first_state.name))
-    else:
-        print("Nothing")
+    for state in states_with_a:
+        print(f"{state.id}: {state.name}")
 
     # Close the session
     session.close()
+
