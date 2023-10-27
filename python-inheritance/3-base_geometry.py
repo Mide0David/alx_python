ca33c0cb3a6aaf37be89783d6654e65559cf6821
mide <midebabalola7@gmail.com>
2023-10-27 14:49:51 +0100

    fixed issue

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,27 +1,11 @@
-"""
-        # Exclude the attribute __init_subclass__
-Custom metaclass to exclude __init_subclass__
-"""
-class BaseGeometryMeta(type):
-        """
-    This metaclass is used to remove the __init_subclass__ method from class attributes.
-    """
-    def __dir__(cls):
-        """
-        Exclude the '__init_subclass__' attribute from class attributes when using dir().
-        """
-        attributes = super().__dir__()
-        return [attribute for attribute in attributes if attribute != "__init_subclass__"]
+#!/usr/bin/python3
 
-# BaseGeometry class with a custom metaclass
-class BaseGeometry(metaclass=BaseGeometryMeta):
-        """
-    The BaseGeometry class serves as a base class with the '__init_subclass__' method excluded.
+"""EMPTY CLASS."""
+
+
+class BaseGeometry:
     """
-    def __dir__(self):
-                """
-        Exclude '__init_subclass__' attribute from instance attributes when using dir().
-        """
-        # Exclude the attribute __init_subclass__
-        attributes = super().__dir__()
-        return [attribute for attribute in attributes if attribute != "__init_subclass__"]
+    an empty class.
+    """
+
+    pass
--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,57 +1,12 @@
-"""
-    This module serves as a foundation for defining various shapes 
-    and their properties and methods
-"""
+#!/usr/bin/python3
 
-class BaseGeometryMeta(type):
-    """
-    This is a metaclass used to represent the class type in order to eliminate
-    the inherited method init subclass
+"""Class based on initial."""
 
-        Exclude attribute init subclass in dir()
 
-    """
-
-    def __dir__(cls):
-                """
-        Exclude attribute init subclass in dir()
-        """
-        attributes = super().__dir__()
-
-        return [
-            attribute for attribute in attributes if attribute != "__init_subclass__"
-        ]
-
-class BaseGeometry(metaclass=BaseGeometryMeta):
-
-    """
-    Base class for geometry-related objects.
-
-    This class serves as the foundation for defining various geometric shapes
-    and their corresponding properties and methods.
-
-    Methods:
-        __dir__(self) -> None
-            Exclude attribute init subclass in dir()
-        area(self) -> None
-            Placeholder method to calculate the area of the geometric shape.
-
-            Raises:
-                Exception: This method is not implemented in the base class and
-                           should be overridden in subclasses to provide specific
-                           implementations for calculating the area of a particular shape.
-    """
-
-     def __dir__(self):
-                 """
-        Exclude attribute init subclass in dir()
-        """
-
-        attributes = super().__dir__()
-
-        return [
-            attribute for attribute in attributes if attribute != "__init_subclass__"
-        ]
+class BaseGeometry:
+    """Presents a function area."""
 
     def area(self):
+        """Raises exception."""
+
         raise Exception("area() is not implemented")
