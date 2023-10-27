d818753ebb836ae6f72bdfc84520338a20cfdee5
mide <midebabalola7@gmail.com>
2023-10-27 14:31:58 +0100

    made some corrections

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -3,48 +3,32 @@
 """
 All cities by state
 """
-import MySQLdb
 import sys
+import MySQLdb
 
-if __name__ == "__main":
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database state_name".format(sys.argv[0]))
-        sys.exit(1)
 
-    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
 
-    try:
-        conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
+    cursor = db.cursor()
 
-        cursor = conn.cursor()
+    sql = """
+    SELECT cities.name
+          FROM states
+          INNER JOIN cities ON states.id = cities.state_id
+          WHERE states.name = %s
+          ORDER BY cities.id ASC
+          """
 
-        query = """
-        USE test_5
-        SELECT GROUP_CONCAT(cities.name ORDER BY cities.id ASC SEPARATOR ', ')
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s;
-        """
+    cursor.execute(sql, (sys.argv[4],))
 
-        cursor.execute(query, (state_name,))
+    data = cursor.fetchall()
 
-        result = cursor.fetchone()[0]
+    print(", ".join([city[0] for city in data]))
 
-        if result:
-            print(result)
-        else:
-            print("State not found or has no cities.")
-
-        cursor.close()
-        conn.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
-        sys.exit(1)
-
+    cursor.close()
+    db.close()
